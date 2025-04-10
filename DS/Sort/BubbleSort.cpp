14037692dd01f810cf063894853d15ffc3345a13
strive <2205487298@qq.com>
2025-04-10 21:35:46 +0800

    initial

---
+++ DS/Sort/BubbleSort.cpp
@@ -0,0 +1,16 @@
+//
+// Created by strive on 2025/4/10.
+//
+#include<iostream>
+using namespace std;
+
+void bubbleSort(int a[],int l,int r){
+
+}
+
+int main()
+{
+    int a[10] = {1,2,3,4,5,6,7,8,9,10};
+    bubbleSort(a,1, 4);
+    return 0;
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+Some Codes On C++.
