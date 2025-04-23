6736362789decb2f8088752be9744d516bce8246
strive <2205487298@qq.com>
2025-04-23 20:12:13 +0800

    update vector

---
+++ DS/Vector/delete.cpp
@@ -0,0 +1,19 @@
+//
+// Created by strive on 2025/4/15.
+//
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void printVector(vector<int>& v) {
+    for(vector<int>::iterator it = v.begin();it != v.end();it++) {
+        cout << *it << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+
+    return 0;
+}
