848ae4ab51f5102b4383ad61e0927dcaab7a514d
Stravey <2205487298@qq.com>
2026-01-14 21:10:18 +0800

    update

---
+++ Algorithm_Base/Subsequence/P1824.cpp
@@ -0,0 +1,41 @@
+//
+// Created by strive on 2026/1/14.
+//
+#include <bits/stdc++.h>
+using namespace std;
+int n,c,x[100005];
+bool check(int dis) {
+    int cnt = 1,place = 0;
+    for(int i = 1;i < n;i++) {
+        if(x[i] - x[place] >= dis) {
+            cnt++;
+            place = i;
+        }
+    }
+    if(cnt >= c) {
+        return true;
+    } else {
+        return false;
+    }
+}
+int main()
+{
+    cin >> n >> c;
+    for(int i = 0;i < n;i++) {
+        cin >> x[i];
+    }
+    sort(x,x + n);
+    int left = 0,right = x[n - 1] - x[0];
+    int ans = 0;
+    while(left < right) {
+        int mid = (left + right) >> 1;
+        if(check(mid)) {
+            ans = mid;
+            left = mid + 1;
+        } else {
+            right = mid;
+        }
+    }
+    cout << ans << endl;
+    return 0;
+}
